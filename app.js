35a680de19a78ea61d248dac698c4bf9f81e5f73
Thorge Hamprecht <thorge.hamprecht@icloud.com>
2023-02-07 15:50:23 +0100

    ff

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const menu = document.querySelector("#mobile-menu")
 const menuLinks = document.querySelector(".navbar__menu")
+const logo = document.querySelector("#navbar__logo")
 
 menu.addEventListener("click", function () {
     menu.classList.toggle("is-active")
     menuLinks.classList.toggle("active")
 });
+
+
+
