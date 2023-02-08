cc59a090c00281710dfc384f916902d41f86d257
Thorge Hamprecht <thorge.hamprecht@icloud.com>
2023-02-08 13:45:51 +0100

    Snake Setup 3.0

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 //board
 var blockSize = 25;
 var rows = 20;
-var columns = 25;
+var columns = 20;
 var board;
 var context;
 
@@ -9,12 +9,13 @@ window.onload = function () {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = columns * blockSize;
-    board.getContext("2d");
+    context = board.getContext("2d");
 
     update();
 }
 
 function update () {
+    console.log(context);
     context.fillStyle = "black";
     context.fillRect(0,0, board.width, board.height);
 }
