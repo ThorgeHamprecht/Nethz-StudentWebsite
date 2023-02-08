c13ed2ce941f30724064941b3ea17f5649f764b5
Thorge Hamprecht <thorge.hamprecht@icloud.com>
2023-02-08 14:27:54 +0100

    Snake Setup 4.0

--- snake.js
+++ snake.js
@@ -1,9 +1,17 @@
 //board
 var blockSize = 25;
 var rows = 20;
-var columns = 20;
+var columns = 25;
 var board;
 var context;
+
+// Snake Head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+// Collectables
+var foodX = blockSize * 8;
+var foodY = blockSize * 9;
 
 window.onload = function () {
     board = document.getElementById("board");
@@ -11,11 +19,35 @@ window.onload = function () {
     board.width = columns * blockSize;
     context = board.getContext("2d");
 
+    placeCollectables();
+    document.addEventListener("keyup", movement);
+
     update();
 }
 
 function update () {
     console.log(context);
-    context.fillStyle = "black";
+    context.fillStyle = "midnight";
     context.fillRect(0,0, board.width, board.height);
+
+    //draw snake
+    context.fillStyle = "lightseagreen";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    //draw collectables
+    context.fillStyle = "salmon";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function placeCollectables() {
+    foodX = Math.floor(Math.random() * columns) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+    while (foodX == snakeX && foodY == snakeY) {
+        foodX = Math.floor(Math.random() * columns) * blockSize;
+        foodY = Math.floor(Math.random() * rows) * blockSize;
+    }
+}
+
+function movement() {
+
 }
