073b40f06583dbcd1bcd5c45c618859c17ab84b2
Thorge Hamprecht <thorge.hamprecht@icloud.com>
2023-02-23 13:43:58 +0100

    Test for dark mode

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const menu = document.querySelector("#mobile-menu")
-const menuLinks = document.querySelector(".navbar__menu")
-const logo = document.querySelector("#navbar__logo")
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const mainContainer = document.querySelector(".main");
+
+var theme = "dark";
+
+window.onload = function () {
+    if (theme === "dark") {
+        mainContainer.toggle("dark");
+    }
+    else {
+        mainContainer.toggle("light");
+    }
+
+
+}
 
 menu.addEventListener("click", function () {
     menu.classList.toggle("is-active")
